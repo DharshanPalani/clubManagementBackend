ce9d3561ed5684bdbb20386f75b15130e15b52cb
DharshanPalani <dharshanpalani.2006@gmail.com>
2025-12-04 23:29:04 +0530

    Domains logic removal

--- modules/domains/IDomain.ts
+++ modules/domains/IDomain.ts
@@ -2,21 +2,14 @@ import type { Request, Response } from "express";
 
 export interface IDomain {
   /**
-  * This function should create new entry in the domains table
-  * @param request - body should contain domain_name along with lead_user_id.
-  * Then the function should return it on successful entry created
-  */
-  addDomain(request : Request, response : Response) : Promise<void>
-
-  /** 
-  * This function should return the domain data like it's name and the lead id with the parms id
-  * */
-  getDomain(request : Request, response : Response) : Promise<void>
+   * This function should create new entry in the domains table
+   * @param request - body should contain domain_name along with lead_user_id.
+   * Then the function should return it on successful entry created
+   */
+  addDomain(request: Request, response: Response): Promise<void>;
 
   /**
-  * This function should return domain data with the lead ID
-  * @param request should contain user data from the authenticateToken middlewhere
-  * User ID in that request should be used to search the domain where the lead is associated
-  */
-  getDomainWithLeadId(request : any, response : Response) : Promise<void>
+   * This function should return the domain data like it's name and the lead id with the parms id
+   * */
+  getDomain(request: Request, response: Response): Promise<void>;
 }
--- modules/members/MemberImp.ts
+++ modules/members/MemberImp.ts
@@ -2,21 +2,6 @@ import type { Request, Response } from "express";
 import type { IMember } from "./IMember.ts";
 import pool from "../../db.ts";
 
-
 export class MembersImp implements IMember {
-  async addMember(request: Request, response: Response): Promise<void> {
-    const { member_id, domain_id } = request.body;
-    try {
-      const result = await pool.query(
-        `INSERT INTO members (member_id , domain_id ) VALUES ($1, $2) RETURNING *`,
-        [member_id, domain_id]
-      );
-
-      response.status(201).json(result.rows[0]);
-
-    } catch(error : any) {
-      response.json(error);
-    }
-  }
-
+  async addMember(request: Request, response: Response): Promise<void> {}
 }
