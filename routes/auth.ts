5a190a64675c689f0e4ca1b5df9878bf0b789888
DharshanPalani <dharshanpalani.2006@gmail.com>
2025-12-11 19:46:07 +0530

    removed old auth code

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,9 +1,5 @@
 import express from "express";
-
-import login from "../modules/auth/login.ts";
-import register from "../modules/auth/register.ts";
 import authenticateToken from "../authenticateToken.ts";
-import me from "../modules/auth/me.ts";
 import { AuthImp } from "../modules/auth/AuthImp.ts";
 
 const authRoute = express.Router();
