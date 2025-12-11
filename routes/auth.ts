d4c7cd993cd80f3377c41064b6433656b8b28011
DharshanPalani <dharshanpalani.2006@gmail.com>
2025-12-11 19:45:40 +0530

    Reworked auth on a class idk

--- routes/auth.ts
+++ routes/auth.ts
@@ -4,11 +4,14 @@ import login from "../modules/auth/login.ts";
 import register from "../modules/auth/register.ts";
 import authenticateToken from "../authenticateToken.ts";
 import me from "../modules/auth/me.ts";
+import { AuthImp } from "../modules/auth/AuthImp.ts";
 
 const authRoute = express.Router();
 
-authRoute.post("/login", login);
-authRoute.post("/register", register);
-authRoute.get("/me", authenticateToken, me);
+const authController = new AuthImp();
+
+authRoute.post("/login", authController.login.bind(authController));
+authRoute.post("/register", authController.register.bind(authController));
+authRoute.get("/me", authenticateToken, authController.me.bind(authController));
 
 export default authRoute;
