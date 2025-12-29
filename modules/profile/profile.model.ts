3fa5b44d7efced9c0aa5be5256f6ed1da42b2986
DharshanPalani <dharshanpalani.2006@gmail.com>
2025-12-29 23:57:42 +0530

    Added model to profile and applied it to repository

---
+++ modules/profile/profile.model.ts
@@ -0,0 +1,8 @@
+export type Profile = {
+  profile_id: number;
+  display_name?: string;
+  phone_no?: string;
+  email?: string;
+  club_id: number;
+  role_id: number;
+};
--- modules/profile/profile.repository.ts
+++ modules/profile/profile.repository.ts
@@ -1,7 +1,12 @@
 import pool from "../../db.ts";
+import { Profile } from "./profile.model.ts";
 
 export class ProfileRepository {
-  async createProfile(user_id: number, club_id: number, role_id: number) {
+  async createProfile(
+    user_id: number,
+    club_id: number,
+    role_id: number,
+  ): Promise<Profile> {
     const query = `INSERT INTO profiles (user_id, club_id, role_id) VALUES($1, $2, $3) RETURNING *`;
     const checkProfileExistance = await pool.query(
       `SELECT 1 FROM profiles WHERE user_id = $1`,
@@ -10,12 +15,12 @@ export class ProfileRepository {
     if (checkProfileExistance.rows.length > 0) {
       throw new Error("Profile already exits for the user ID: " + user_id);
     }
-    const { rows } = await pool.query(query, [user_id, club_id, role_id]);
+    const profile = await pool.query(query, [user_id, club_id, role_id]);
 
-    return rows;
+    return profile.rows[0];
   }
 
-  async getProfileWithUserID(user_id: number) {
+  async getProfileWithUserID(user_id: number): Promise<Profile> {
     const query = `SELECT * FROM profiles WHERE user_id = $1`;
     const profileData = await pool.query(query, [user_id]);
 
